151ddee947cb26b51c9ef37bdb86bd50a7de6deb
lilly <lilly1987@naver.com>
2021-04-13 15:32:57 +0900

    1

---
+++ COM3D2.Lilly.BepInExPlugin/InfoPatch/TBodyPatch.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COM3D2.Lilly.Plugin
+{
+    class TBodyPatch
+    {
+        //TBody
+
+        [HarmonyPostfix,HarmonyPatch(typeof(TBody), "BoneMorph_FromProcItem")]        
+        public static void BoneMorph_FromProcItem(string tag, float f)
+        {
+            MyLog.LogMessage("TBody.BoneMorph_FromProcItem"
+                , tag
+                ,f
+                );
+        }
+        [HarmonyPostfix,HarmonyPatch(typeof(TBody), "VertexMorph_FromProcItem")]        
+        public static void VertexMorph_FromProcItem(string tag, float f)
+        {
+            MyLog.LogMessage("TBody.VertexMorph_FromProcItem"
+                ,tag
+                ,f
+                );
+        }
+        
+        [HarmonyPostfix,HarmonyPatch(typeof(TBody), "UpdateMyBoneMorph")]        
+        public static void UpdateMyBoneMorph(string name, float val)
+        {
+            MyLog.LogMessage("TBody.UpdateMyBoneMorph"
+                , name
+                , val
+                );
+        }
+
+
+    }
+}
--- COM3D2.Lilly.BepInExPlugin/Utill/HarmonyUtill.cs
+++ COM3D2.Lilly.BepInExPlugin/Utill/HarmonyUtill.cs
@@ -65,6 +65,7 @@ namespace COM3D2.Lilly.Plugin
             infoList.Add(typeof(CharacterMgrPatch));// 프리셋값 출력용
             infoList.Add(typeof(MaidPatch));// 아이템 장착 확인용
             infoList.Add(typeof(ScheduleMgrPatch));// 스케줄 관리
+            infoList.Add(typeof(TBodyPatch));// 스케줄 관리
             //infoList.Add(typeof(FullBodyIKMgrPatch));// 뼈 관련. 안뜨는거 같음
         }
 
